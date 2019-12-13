9a6ca6f4fce88803a46dbc18e3c092b4bd66503a
Carlos Saraiva <carlosedosaraiva@gmail.com>
2019-12-13 10:40:16 -0300

    Update Statics componenet to always calculate grid dimensions

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,15 +7,15 @@ function Statistics({ show }) {
   const [countGutters, setCountGutters] = useState(0);
   const [widthGutters, setWidthGutters] = useState(0);
 
-  const [space, setSpace] = useState(0)
+  const [space, setSpace] = useState(0);
 
   document.getElementsByTagName("body")[0].onresize = function() {
-    show && updateColumnsAndGutter()
+    updateColumnsAndGutter();
   };
 
   useEffect(() => {
-    show && updateColumnsAndGutter()
-  })
+    updateColumnsAndGutter();
+  });
 
   const updateColumnsAndGutter = () => {
     const allColumns = document.querySelectorAll(".grid-overlay-column");
@@ -37,25 +37,33 @@ function Statistics({ show }) {
     setCountColumns(visibleColumns);
     setWidthColumns(allColumns[0].clientWidth);
 
-    setCountColumns(visibleColumns)
-    setWidthColumns(allColumns[0].clientWidth)
+    setCountColumns(visibleColumns);
+    setWidthColumns(allColumns[0].clientWidth);
 
-    setCountGutters(auxCountGutters)
-    setWidthGutters(getComputedStyle(allColumns[1], null).marginLeft)
+    setCountGutters(auxCountGutters);
+    setWidthGutters(getComputedStyle(allColumns[1], null).marginLeft);
 
-    const utilArea = Number.parseInt(getComputedStyle(document.querySelector(".u-relative"), null).width)
-    const totalArea = Number.parseInt(getComputedStyle(document.querySelector("#root"), null).width)
-    setSpace(totalArea - utilArea)
-  }
+    const utilArea = Number.parseInt(
+      getComputedStyle(document.querySelector(".u-relative"), null).width
+    );
+    const totalArea = Number.parseInt(
+      getComputedStyle(document.querySelector("#root"), null).width
+    );
+    setSpace(totalArea - utilArea);
+  };
 
   return (
-    show && (
-      <div className="statistics-container">
-        <div>São <b>{countColumns}</b> colunas com <b>{widthColumns}px</b> cada</div>
-        <div>São <b>{countGutters}</b> gutters com <b>{widthGutters}</b> cada</div>
-        <div>O espaço que está sobrando é de <b>{space}px</b> no total</div>
+    <div className="statistics-container">
+      <div>
+        São <b>{countColumns}</b> colunas com <b>{widthColumns}px</b> cada
       </div>
-    )
+      <div>
+        São <b>{countGutters}</b> gutters com <b>{widthGutters}</b> cada
+      </div>
+      <div>
+        O espaço que está sobrando é de <b>{space}px</b> no total
+      </div>
+    </div>
   );
 }
 
