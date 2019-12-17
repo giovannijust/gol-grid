63e481d5a8a50759cc5747685d749cdc29c18c2a
Carlos Saraiva <carlosedosaraiva@gmail.com>
2019-12-17 10:40:42 -0300

    Generaly layout adjustments

--- src/App.js
+++ src/App.js
@@ -47,56 +47,65 @@ function App() {
           <div className="introduction-content">
             <h1 style={{ textAlign: "center" }}>Teste de grid</h1>
             <h3>Introdução</h3>
-            <p>
-              Site com teste de grid para validar layout e breakpoints. Há dois
-              arquivos de estilos csonfigurados: Um "responsivo" e outro "fixo"
-              a qual é possível mudar pressionando os botões do menu acima. O
-              grid é uma solução customizada usando Sass + Postcss gerando
-              colunas usando a técnica mais tradicional com "floats" e
-              "margins". Dependendo de qual browser e versões o publico da GOL
-              usa, podemos adotar técnicas mais modernas e simples para
-              desenvolvimento de Grid. Por enquanto estamos usando um método
-              mais tradicional pensando no pior cenário e priorizando
-              compatibilidade.
-            </p>
+            <div className="introduction-text">
+              <p>
+                Site com teste de grid para validar layout e breakpoints. Há
+                dois arquivos de estilos csonfigurados: Um "responsivo" e outro
+                "fixo" a qual é possível mudar pressionando os botões do menu
+                acima. O grid é uma solução customizada usando Sass + Postcss
+                gerando colunas usando a técnica mais tradicional com "floats" e
+                "margins". Dependendo de qual browser e versões o publico da GOL
+                usa, podemos adotar técnicas mais modernas e simples para
+                desenvolvimento de Grid. Por enquanto estamos usando um método
+                mais tradicional pensando no pior cenário e priorizando
+                compatibilidade.
+              </p>
+            </div>
           </div>
+          <hr />
           <div className="introduction-content">
             <h3>Layout Fixo</h3>
-            <p>
-              Grids configurado conforme especificação da TV1. Ao pressionar o
-              botão "Responsivo", outra configuração de layout aplicado. Ele
-              consiste em colunas fluidas mas ainda baseada no conceito proposto
-              acima. Portanto em mobile há 2 colunas e tambem manatemos o
-              breakpoint do "Grid Layout" apenas para referencia. A partir deste
-              breakpoint, a layout trabalha com 12 colunas se extendo até o
-              limite máximo de largura da tela de 1140px. Essa largura máxima
-              pode ser ampliada se necessário. Por enquanto estamos baseando no
-              comportamento do layout proposto pela TV1.
-            </p>
-            <ul>
-              Breakpoints:
-              <li>367px</li>
-              <li>988px (desktop light)</li>
-              <li>1140</li>
-            </ul>
+            <div className="introduction-text">
+              <p>
+                Grids configurado conforme especificação da TV1. Ao pressionar o
+                botão "Responsivo", outra configuração de layout aplicado. Ele
+                consiste em colunas fluidas mas ainda baseada no conceito
+                proposto acima. Portanto em mobile há 2 colunas e tambem
+                manatemos o breakpoint do "Grid Layout" apenas para referencia.
+                A partir deste breakpoint, a layout trabalha com 12 colunas se
+                extendo até o limite máximo de largura da tela de 1140px. Essa
+                largura máxima pode ser ampliada se necessário. Por enquanto
+                estamos baseando no comportamento do layout proposto pela TV1.
+              </p>
+              <ul>
+                Breakpoints:
+                <li>367px</li>
+                <li>988px (desktop light)</li>
+                <li>1140</li>
+              </ul>
+            </div>
           </div>
+          <hr />
           <div className="introduction-content">
             <h3>Layout Responsivo</h3>
-            <p>
-              O layout responsivo ele deixa as colunas fluidas enquanto ocupa
-              toda a largura da tela. Esse grid precisa de validação e pode-se
-              definir algumas coisas como por exemplo a largura total por
-              breakpoint. Mantivemos o breakpoint "desktop light" para
-              comparação.
-            </p>
-            <ul>
-              Breakpoints:
-              <li>367px</li>
-              <li>988px (desktop light)</li>
-              <li>1140</li>
-            </ul>
+            <div className="introduction-text">
+              <p>
+                O layout responsivo ele deixa as colunas fluidas enquanto ocupa
+                toda a largura da tela. Esse grid precisa de validação e pode-se
+                definir algumas coisas como por exemplo a largura total por
+                breakpoint. Mantivemos o breakpoint "desktop light" para
+                comparação.
+              </p>
+              <ul>
+                Breakpoints:
+                <li>367px</li>
+                <li>988px (desktop light)</li>
+                <li>1140</li>
+              </ul>
+            </div>
           </div>
         </section>
+        <hr />
         <section className="showcase">
           <div className="showcase-introduction">
             <h2 style={{ textAlign: "center" }}>Showcase</h2>
@@ -107,9 +116,13 @@ function App() {
             </p>
           </div>
           <div className="showcase-demonstration">
-            <h3 style={{ textAlign: "center" }}>Card com imagem + texto</h3>
+            <div className="l-container">
+              <h3 style={{ textAlign: "center" }}>Card com imagem + texto</h3>
+            </div>
             <Card />
-            <h3 style={{ textAlign: "center" }}>Galeria de imagens</h3>
+            <div className="l-container">
+              <h3 style={{ textAlign: "center" }}>Galeria de imagens</h3>
+            </div>
             <Gallery />
           </div>
         </section>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,17 @@ import React from "react";
 
 function Card() {
   return (
-    <div className="card">
-      <div className="card-image"></div>
-      <div className="card-text">
-        <p>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+    <div className="l-container">
+      <div className="card">
+        <div className="card-image"></div>
+        <div className="card-text">
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
     </div>
   );
